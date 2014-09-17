fbbcb91b6cec1789930aa0554ec94f978eed50be
Kim Desrosiers <kimdesro@gmail.com>
2014-09-16 21:29:48 -0400

    modif to bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
+echo 'Vimfiles setup begin'
+
 if [ -d "~/.vim" ]; then
-    # Control will enter here if $DIRECTORY exists.
     mv ~/.vim ~/.vim_old
+    echo '.vim_old have been created'
 fi
 
 if [ -f "~/.vimrc" ]; then
     mv ~/.vimrc ~/.vimrc_old
+    echo '.vimrc_old have been created'
 fi
 
 #Installing the base directory of the vim files
 cd ~/
 git clone https://github.com/kimond/vimfiles.git .vim
 
-ln -s .vim/.vimrc .vimrc
+ln -sf .vim/.vimrc .vimrc
 
 cd .vim
 git submodule update --init
@@ -21,4 +24,4 @@ git submodule update --init
 #Install bundle with vundle
 vim +BundleInstall +qall
 
-success 'Setup complete. Run vim and enjoy'
+echo 'Setup complete. Run vim and enjoy'
